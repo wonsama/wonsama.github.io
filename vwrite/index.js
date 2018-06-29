219c6f322076db2627ad5243f45f0e0f65efb46e
wonsama <imtough99@gmail.com>
2018-06-29 14:49:01 +0900

    .

--- vwrite/index.js
+++ vwrite/index.js
@@ -2,7 +2,7 @@
 https://steemconnect.com/apps/@wonsama.quiz/edit
 */
 const URL_LOCAL = 'file:///Users/wonsama/Documents/GitHub/wonsama.github.io/vwrite/index.html';
-const URL_GITHUB = 'https://wonsama.github.io/vwrite/index.html';
+const URL_GITHUB = 'https://wonsama.github.io/vwrite/';
 
 var api = sc2.Initialize({
   app: 'wonsama.quiz',
@@ -15,3 +15,7 @@ let state = '1,2,3';
 var link = api.getLoginURL(state);
 
 console.log(link)
+
+
+
+https://steemconnect.com/oauth2/authorize?client_id=wonsama.quiz&redirect_uri=https%3A%2F%2Fwonsama.github.io%2Fvwrite%2F&scope=vote,comment
