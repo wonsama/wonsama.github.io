b27d21b8197c754f4c7a060f0aef3df77ce5798d
wonsama <imtough99@gmail.com>
2019-04-08 15:49:19 +0900

    Updates

---
+++ precache-manifest.3205f1a92e5a8b9259a6f2b8fc32b10b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e44b47789e67861c4ead",
+    "url": "/wallet/static/css/main.d7cb3bbc.chunk.css"
+  },
+  {
+    "revision": "e44b47789e67861c4ead",
+    "url": "/wallet/static/js/main.75e390d8.chunk.js"
+  },
+  {
+    "revision": "4313a70b9113ec012757",
+    "url": "/wallet/static/js/runtime~main.ee2bcf40.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/wallet/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wallet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9e24abf34fdd2bf97d6d2370eab7a58b",
+    "url": "/wallet/index.html"
+  }
+];
